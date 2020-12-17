578bd21ff144d65c3911c99e9cfc5d4800096e05
Your Name <you@example.com>
2020-12-17 19:50:30 +0800

    20201217-deployConfig

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
   },
-  // router: {
-  //   base: '/feiyami/'
-  // }
+  router: {
+    base: '/feiyami/'
+  }
 }
