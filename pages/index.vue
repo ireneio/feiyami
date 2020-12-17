ff651cf2050f09071a8a86ac5c988bb920874ec0
Your Name <you@example.com>
2020-12-17 19:24:25 +0800

    20201217-final

---
+++ pages/index.vue
@@ -0,0 +1,403 @@
+<template>
+  <div class="container">
+   <header class="header">
+     VIP
+   </header>
+   <body class="body">
+     <div class="banner banner--lg">
+       <div class="banner__icon banner__icon--1"></div>
+       <h2 class="banner__title banner__title--main">普通会员</h2>
+       <h5 class="banner__subtitle banner__subtitle--main">每日可领取3次任务</h5>
+       <p class="banner__text banner__text--main">2020.08.11止</p>
+     </div>
+     <div class="banner banner--md banner--md1" @click="toggleDetail = true">
+       <div class="banner__left">
+        <h2 class="banner__title">白银会员</h2>
+        <h5 class="banner__subtitle" v-show="toggleDetail">每日可领取10次任务</h5>
+        <p class="banner__text" v-show="toggleDetail">[1.20元/条]</p>
+       </div>
+       <div class="banner__right">
+         <div class="banner__money" :class="{ 'banner__money--spacedSmall': !toggleDetail }">￥330.00</div>
+         <button class="btn btn--primary" v-show="toggleDetail" @click="toggleDialog = true">立即加入</button>
+       </div>
+     </div>
+     <div class="banner banner--md banner--md2">
+       <div class="banner__left">
+        <h2 class="banner__title">黄金会员</h2>
+        <h5 class="banner__subtitle">每日可领取19次任务</h5>
+       </div>
+       <div class="banner__right">
+         <div class="banner__money banner__money--spaced">￥660.00</div>
+       </div>
+     </div>
+     <div class="banner banner--md banner--md3">
+       <div class="banner__left">
+        <h2 class="banner__title">铂金会员</h2>
+        <h5 class="banner__subtitle">每日可领取28次任务</h5>
+       </div>
+       <div class="banner__right">
+         <div class="banner__money">￥990.00</div>
+       </div>
+     </div>
+     <div class="banner banner--md banner--md4">
+       <div class="banner__left">
+        <h2 class="banner__title">钻石会员</h2>
+        <h5 class="banner__subtitle">每日可领取99次任务</h5>
+       </div>
+       <div class="banner__right">
+         <div class="banner__money">￥2990.00</div>
+       </div>
+     </div>
+     <div class="banner banner--md banner--md5">
+       <div class="banner__left">
+        <h2 class="banner__title">荣耀会员</h2>
+        <h5 class="banner__subtitle">每日可领取228次任务</h5>
+       </div>
+       <div class="banner__right">
+         <div class="banner__money">￥5940.00</div>
+       </div>
+     </div>
+     <div class="banner banner--md banner--md6">
+       <div class="banner__left">
+        <h2 class="banner__title">尊耀会员</h2>
+        <h5 class="banner__subtitle">每日可领取338次任务</h5>
+       </div>
+       <div class="banner__right">
+         <div class="banner__money">￥9899.00</div>
+       </div>
+     </div>
+   </body>
+   <footer class="footer">
+     <div class="footerItem">
+       <div class="footerItem__icon footerItem__icon--1"></div>
+       <div class="footerItem__text">任務</div>
+     </div>
+      <div class="footerItem">
+       <div class="footerItem__icon footerItem__icon--2"></div>
+       <div class="footerItem__text">任務</div>
+     </div>
+      <div class="footerItem">
+       <div class="footerItem__icon footerItem__icon--3"></div>
+       <div class="footerItem__text">VIP</div>
+     </div>
+      <div class="footerItem">
+       <div class="footerItem__icon footerItem__icon--4"></div>
+       <div class="footerItem__text">我的</div>
+     </div>
+   </footer>
+   <button class="floatingBtn">
+     <div class="floatingBtn__icon"></div>
+   </button>
+   <div class="mask" v-show="toggleDialog"></div>
+   <!-- :class="{ 'mask--show': toggleDialog }" -->
+   <div class="dialog" v-show="toggleDialog">
+     <!-- :class="{ 'dialog--show': toggleDialog }" -->
+     <div class="dialog__title">提示</div>
+     <p class="dialog__text">確定花費330.00元 <br /> 成為12個月的白银會員嗎?</p>
+     <div class="dialog__btnBox">
+       <button class="btn btn--secondary btn--fat" @click="toggleDialog = false">取消</button>
+       <button class="btn btn--conseq btn--primary2 btn--fat" @click="toggleDialog = false">确定</button>
+     </div>
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      toggleDialog: false,
+      toggleDetail: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  border-radius: 40px;
+  padding: 10px 18px;
+  outline: none;
+  border: none;
+  font-size: 16px;
+  cursor: pointer;
+  &--primary {
+    color: #8A56AC;
+    background-color: #fff;
+  }
+  &--primary2 {
+    background-color: #8A56AC;
+    color: #fff;
+  }
+  &--secondary {
+    background-color: #9599B3;
+    color: #fff;
+  }
+  &--conseq {
+    margin-left: 12px;
+  }
+  &--fat {
+    padding-left: 31px;
+    padding-right: 31px;
+    padding-top: 12px;
+    padding-bottom: 12px;
+  }
+}
+.floatingBtn {
+  position: absolute;
+  bottom: calc(44px + 64px);
+  right: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 52px;
+  height: 52px;
+  border-radius: 50%;
+  border: none;
+  outline: none;
+  background-color: #8A56AC;
+  cursor: pointer;
+  &__icon {
+    width: 30px;
+    height: 30px;
+    background-size: contain;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-image: url(/4.svg);
+  }
+}
+.mask {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 4;
+  background-color: #000000CC;
+}
+.dialog {
+  min-width: 327px;
+  position: absolute;
+  top: 220px;
+  left: 50%;
+  z-index: 5;
+  transform: translate(-50%,0);
+  border-top-right-radius: 8px;
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 65px;
+  background-color: #fff;
+  padding-left: 32px;
+  padding-top: 24px;
+  padding-bottom: 24px;
+  padding-right: 24px;
+  transition: opacity .3s;
+  &__title {
+    font-size: 26px;
+    color: #000;
+    font-weight: bold;
+  }
+  &__text {
+    font-size: 14px;
+    color: #998FA2;
+    margin-top: 10px;
+    margin-bottom: 28px;
+  }
+  &__btnBox {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+  }
+}
+
+.container {
+  width: 100vw;
+  overflow-x: hidden;
+}
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 44px;
+  background-color: #fff;
+  color: #000;
+  font-size: 1rem;
+}
+.body {
+  width: 100vw;
+  height: calc(100vh - 44px - 64px);
+  overflow: auto;
+}
+.banner {
+  border-bottom-left-radius: 65px;
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  &--lg {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 183px;
+    width: 100vw;
+    background-color: rgba(138, 86, 172, 1);
+    background-image: url(/bg1.png);
+  }
+  &--md {
+    position: relative;
+    display: flex;
+    padding-left: 56px;
+    padding-right: 22px;
+    padding-top: 33px;
+    padding-bottom: 33px;
+    &:before {
+      content: '';
+      display: block;
+      width: 60px;
+      height: 60px;
+      position: absolute;
+      top: -60px;
+      left: 0;
+      z-index: -1;
+    }
+  }
+  &--md1,
+  &--md3,
+  &--md5 {
+    &:before {
+      background-color: #352641;
+    }
+  }
+  &--md2,
+  &--md4,
+  &--md6 {
+    &:before {
+      background-color: #241332;
+    }
+  }
+  &--md1 {
+    background-color: #352641;
+    background-image: url(/bg2.png);
+  }
+  &--md2 {
+    background-color: #241332;
+    background-image: url(/bg3.png);
+  }
+  &--md3 {
+    background-color: #352641;
+    background-image: url(/bg4.png);
+  }
+  &--md4 {
+    background-color: #241332;
+    background-image: url(/bg6.png);
+  }
+  &--md5 {
+    background-color: #352641;
+    background-image: url(/bg5.png);
+  }
+  &--md6 {
+    background-color: #241332;
+    background-image: url(/bg7.png);
+  }
+  &__right {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    margin-left: auto;
+  }
+  &__money {
+    margin-right: 9px;
+    margin-bottom: 11px;
+    color: #fff;
+    font-weight: bold;
+    font-size: 20px;
+    color: rgba(255,255,255, .6);
+    &--spaced {
+      margin-top: 33px;
+    }
+    &--spacedSmall {
+      margin-top: 6px;
+    }
+  }
+  &__icon,
+  &__title,
+  &__subtitle,
+  &__text {
+    color: rgba(255,255,255, .6);
+    &--main {
+      color: #fff;
+    }
+  }
+  &__icon,
+  &__title,
+  &__subtitle {
+    margin-bottom: 4px;
+  }
+  &__subtitle,
+  &__text {
+    opacity: .72;
+    font-weight: 100;
+  }
+  &__icon {
+    width: 60px;
+    height: 60px;
+    background-size: contain;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-image: url(/ic1.svg);
+    
+  }
+  &__title {
+    font-size: 26px;
+    font-weight: bold;
+  }
+  &__subtitle {
+    font-size: 14px;
+    font-weight: 100;
+  }
+  &__text {
+    font-size: 1rem;
+  }
+}
+.footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  width: 100vw;
+  height: 64px;
+  background-color: #fff;
+  color: #717171;
+}
+.footerItem {
+  flex: 0 0 25%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 12px;
+  &__icon {
+    width: 22px;
+    height: 20px;
+    background-size: contain;
+    background-position: center center;
+    background-repeat: no-repeat;
+    margin-bottom: 8px;
+    &--1 {
+      background-image: url(/2.svg);
+    }
+    &--2 {
+      background-image: url(/3.svg);
+    }
+    &--3 {
+      background-image: url(/1.svg);
+    }
+    &--4 {
+      background-image: url(/3.svg);
+    }
+  }
+  &__text {
+    color: #717171;
+  }
+}
+</style>
